458f26c32167dbf23f3da8260cfcad2e879fadfc
Fernando Renteria <rentfern@gmail.com>
2018-01-05 19:46:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do | season |
-    puts season.to_s
+    puts "#{season.to_s}"
     season.each do | holiday, supplies |
       puts holiday.to_s
       supplies.each do | supply |
