446a4ddfb648dc1ed88ddd7d1f9c3cddec901509
Fernando Renteria <rentfern@gmail.com>
2018-01-05 20:16:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,7 +103,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do | season, holidays |
     holidays.each do | holiday, supplies |
       supplies.select do | supply |
-        
+        supply == "BBQ"
       end
     end
   end
